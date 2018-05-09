74fd427ea975adfb98da1b7832e7ae414f448c8f
Krzysztof <krzyskin@wp.pl>
2018-05-09 23:52:15 +0200

    style changed

--- js/weather.jsx
+++ js/weather.jsx
@@ -29,8 +29,8 @@ class Form extends React.Component {
     render() {
         return (
             <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="city..."/>
-                <input type="text" name="country" placeholder="country..."/>
+                <input type="text" name="city" placeholder=" City . . ."/>
+                <input type="text" name="country" placeholder=" Country . . ."/>
                 <button>Get Weather</button>
 
             </form>
